680b7d7500ca825e18a9f9c3666ecc1c24e40a85
razserv2010 <47921926+razserv2010@users.noreply.github.com>
2026-07-05 22:41:55 +0300

    Update switcher-boiler-card.js

--- switcher-boiler-card.js
+++ switcher-boiler-card.js
@@ -464,7 +464,7 @@ class SwitcherBoilerCard extends LitElement {
 
               <g direction="ltr">
                 <text
-                  x="295"
+                  x="260"
                   y="172"
                   text-anchor="end"
                   font-size="12"
